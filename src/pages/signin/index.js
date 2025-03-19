88316f13ebb438c2de443f6491f437570eb06b8e
mando <143672248+Sumandosilalahi2929@users.noreply.github.com>
2025-03-19 20:43:34 +0700

    feat: add login & instal package

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,51 +1,75 @@
 import React, { useState } from "react";
 import { Container, Card } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
-import { Button } from "react-bootstrap";
-// import SButton from "../../component/Button";
+import TextInputWithLabel from "../../component/TextInputWithLabel";
+import SButton from "../../component/Button";
+import axios from "axios";
+import SAlert from "../../component/Alert";
 
 export default function PageSingnin() {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+  const [alert, setAlert] = useState({
+    message: "",
+    type: "",
+  });
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:9000/api/v1/cms/auth/signin",
+        {
+          email: form.email,
+          password: form.password,
+        }
+      );
+
+      setAlert({ message: "Login berhasil!", type: "success" });
+      console.log(res);
+    } catch (err) {
+      setAlert({
+        message: err.response?.data?.message || "Terjadi kesalahan!",
+        type: "danger",
+      });
+    }
+  };
+
   return (
     <Container className="d-flex justify-content-center align-items-center vh-100">
       <Card style={{ width: "22rem", padding: "1 rem" }}>
         <Card.Body>
+          {/* Tampilkan Alert Jika Ada */}
+          {alert.message && (
+            <SAlert message={alert.message} type={alert.type} />
+          )}
           <Card.Title>Login</Card.Title>
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                name="email"
-                value={form.email}
-                type="email"
-                placeholder="Enter email"
-                onChange={handleChange}
-              />
-            </Form.Group>
+            <TextInputWithLabel
+              label="Email"
+              name="email"
+              value={form.email}
+              type="email"
+              placeholder="Enter email"
+              onChange={handleChange}
+            />
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                name="password"
-                value={form.password}
-                type="password"
-                placeholder="Password"
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
+            <TextInputWithLabel
+              label="Password"
+              name="password"
+              value={form.password}
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
           </Form>
-          <Button variant="primary" className="w-100">
+          <SButton action={handleSubmit} variant="primary" className="w-100">
             login
-          </Button>
+          </SButton>
         </Card.Body>
       </Card>
     </Container>
